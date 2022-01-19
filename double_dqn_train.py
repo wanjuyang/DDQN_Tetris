8133612293414b6356cfddf6710170c77c378242
wanjuyang <emily039056@gmail.com>
2022-01-19 18:30:41 +0800

    double_dqn

--- double_dqn_train.py
+++ double_dqn_train.py
@@ -32,7 +32,7 @@ def get_args():
     parser.add_argument("--save_interval", type=int, default=1000)
     parser.add_argument("--replay_memory_size", type=int, default=30000,
                         help="Number of epoches between testing phases")
-    parser.add_argument("--log_path", type=str, default="tensorboard")
+    parser.add_argument("--log_path", type=str, default="tensorboards")
     parser.add_argument("--saved_path", type=str, default="trained_models")
 
     args = parser.parse_args()
@@ -50,14 +50,15 @@ def train(opt):
     # os.makedirs(opt.log_path)
     writer = SummaryWriter(opt.log_path)
     env = Tetris(width=opt.width, height=opt.height, block_size=opt.block_size)
-    # model = DeepQNetwork()
-    model = Dueling_DeepQNetwork()
+    model = DeepQNetwork()
+    target_model = DeepQNetwork()
     optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr)
     criterion = nn.MSELoss()
 
     state = env.reset()
     if torch.cuda.is_available():
         model.cuda()
+        target_model.cuda()
         state = state.cuda()
 
     replay_memory = deque(maxlen=opt.replay_memory_size)
@@ -105,6 +106,9 @@ def train(opt):
             continue
         if len(replay_memory) < opt.replay_memory_size / 10:
             continue
+        
+        target_model.load_state_dict(model.state_dict())
+        
         epoch += 1
         batch = sample(replay_memory, min(len(replay_memory), opt.batch_size))
         state_batch, reward_batch, next_state_batch, done_batch = zip(*batch)
@@ -119,13 +123,16 @@ def train(opt):
 
         q_values = model(state_batch)
         model.eval()
+        
+        ##DDQN
         with torch.no_grad():
             next_prediction_batch = model(next_state_batch)
+            target_prediction_batch = target_model(next_state_batch)
         model.train()
 
         y_batch = torch.cat(
             tuple(reward if done else reward + opt.gamma * prediction for reward, done, prediction in
-                  zip(reward_batch, done_batch, next_prediction_batch)))[:, None]
+                  zip(reward_batch, done_batch, target_prediction_batch)))[:, None]
 
         optimizer.zero_grad()
         loss = criterion(q_values, y_batch)
@@ -139,14 +146,14 @@ def train(opt):
             final_score,
             final_tetrominoes,
             final_cleared_lines))
-        writer.add_scalar('Train/Dueling_DQN_Score', final_score, epoch - 1)
-        writer.add_scalar('Train/Dueling_DQN_Tetrominoes', final_tetrominoes, epoch - 1)
-        writer.add_scalar('Train/Dueling_DQN_Cleared lines', final_cleared_lines, epoch - 1)
+        writer.add_scalar('Train/Double_DQN_Score', final_score, epoch - 1)
+        writer.add_scalar('Train/Double_DQN_Tetrominoes', final_tetrominoes, epoch - 1)
+        writer.add_scalar('Train/Double_DQN_Cleared lines', final_cleared_lines, epoch - 1)
 
         if epoch > 0 and epoch % opt.save_interval == 0:
-            torch.save(model, "{}/Dueling_DQN_tetris_{}".format(opt.saved_path, epoch))
+            torch.save(model, "{}/Double_DQN_tetris_{}".format(opt.saved_path, epoch))
 
-    torch.save(model, "{}/Dueling_DQN_tetris".format(opt.saved_path))
+    torch.save(model, "{}/Double_DQN_tetris".format(opt.saved_path))
 
 
 if __name__ == "__main__":
